8a7f22219d80ddb4b6b8197541580bf4124ac4d4
N Sparenga <nrs87@users.noreply.github.com>
2014-11-04 05:38:01 -0700

    Initial draft of home automation widget.

---
+++ js/widget.js
@@ -0,0 +1,7 @@
+/*
+ * Dev Skills Widget - Rocket Media
+ * Derived From https://github.com/rocket-media/dev-skills-activity
+ */
+
+// Ensure when the page loads, that the widget displays
+$('#widget').modal('show');
